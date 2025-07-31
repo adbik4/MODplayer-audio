491f119af51d5492aa06ca8c4dd81c93e3c1e9de
adbik4 <abik@student.agh.edu.pl>
2025-08-01 00:44:50 +0200

    started working on the player

--- modplayer.py
+++ modplayer.py
@@ -1,6 +1,26 @@
-from modformat import Loader
+import pyaudio
+import time
+import numpy as np
+import modformat
+
+CHUNK = 1024
 filepath = "examples/the_objttze.mod"
 
-loader = Loader()
+# Initialize pyAudio
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paUInt8, channels=1, rate=8287, input=False, output=True)
+
+# Load song
+loader = modformat.Loader()
 song = loader.loadSong(filepath)
 print(song)
+
+# Play all of its samples
+for sample in song.samplelist:
+    audio_data = np.array(sample.data).view(np.int8)[::4]
+    stream.write(audio_data.tobytes())
+
+# Close the stream and terminate pyAudio
+stream.stop_stream()
+stream.close()
+p.terminate()
