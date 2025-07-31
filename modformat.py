7e399b30e9bc6baf46c1acf67df7c0567e6cad7e
adbik4 <abik@student.agh.edu.pl>
2025-08-01 00:44:34 +0200

    cleanup

--- modformat.py
+++ modformat.py
@@ -87,6 +87,8 @@ class Song :    # holds all of the information from a .MOD file
 
 # an object which can read a given .MOD file
 class Loader:
+    __all__ = ["loadSong"]
+    
     # public method
     def loadSong(self, filepath: str):
         f = open(filepath, "rb")
@@ -99,7 +101,7 @@ class Loader:
         self._song_length = 0
         # ----
         
-        magic = self.__readMagic(f)
+        magic = self._readMagic(f)
         if (magic == "\0\0\0\0"):
             self.max_sample_count = 15
         elif (magic == "M.K."):
@@ -109,76 +111,76 @@ class Loader:
         else:
             self.max_sample_count = 128
             
-        name = self.__readSongName(f)
-        samplelist = self.__loadSampleData(f)
-        length = self.__readSongLength(f)
-        repeat_idx = self.__readSearchUntil(f)
-        pattern_order = self.__loadPatternPositions(f)
-        patternlist = self.__loadPatternData(f)
+        name = self._readSongName(f)
+        samplelist = self._loadSampleData(f)
+        length = self._readSongLength(f)
+        repeat_idx = self._readSearchUntil(f)
+        pattern_order = self._loadPatternPositions(f)
+        patternlist = self._loadPatternData(f)
 
         f.close()
         return Song(name, samplelist, length, repeat_idx, pattern_order, patternlist)
         
     # private methods:
     # ---- file operations
-    def __toString(self, data: bytes) -> str:
+    def _toString(self, data: bytes) -> str:
         return data.translate(None, b'\0').decode("CP437")
 
-    def __toInt_LE(self, data:bytes) -> int:
+    def _toInt_LE(self, data:bytes) -> int:
         return int.from_bytes(data, "little")
 
-    def __toInt_BE(self, data:bytes) -> int:
+    def _toInt_BE(self, data:bytes) -> int:
         return int.from_bytes(data, "big")
 
-    def __readBlock(self, f: BufferedReader, offset: int, length: int) -> bytes:
+    def _readBlock(self, f: BufferedReader, offset: int, length: int) -> bytes:
         f.seek(offset)
         return f.read(length)
 
     # ---- data processing
 
     # extract a smaller sequence of bits from a bytes object. Returns an int
-    def __extractBits(self, data: bytes, start: int, end: int) -> int:
+    def _extractBits(self, data: bytes, start: int, end: int) -> int:
         wordlen = len(data) * 8
         if (start < wordlen and start >= 0) and (end < wordlen and end >= 0) and (start <= end):
-            value = self.__toInt_BE(data)
+            value = self._toInt_BE(data)
             shifted = value >> (wordlen - end - 1)
             result = shifted & (2**(end - start + 1) - 1)
             return result
         return -1
 
     # unpacks raw note data
-    def __extractNoteInfo(self, data: bytes) -> tuple[int, int, int]:
-        sample = (self.__extractBits(data, 0, 3) << 4) + self.__extractBits(data, 15, 19)
-        period = self.__extractBits(data, 4, 15)
-        effect = self.__extractBits(data, 20, 31)
+    def _extractNoteInfo(self, data: bytes) -> tuple[int, int, int]:
+        sample = (self._extractBits(data, 0, 3) << 4) + self._extractBits(data, 15, 19)
+        period = self._extractBits(data, 4, 15)
+        effect = self._extractBits(data, 20, 31)
         return sample, period, effect
 
     # ---- data structure operations
 
     # name of the song
-    def __readSongName(self, f: BufferedReader) -> str:
-        data = self.__readBlock(f, SONGNAME_OFFSET, SONGNAME_LEN)
-        return self.__toString(data)
+    def _readSongName(self, f: BufferedReader) -> str:
+        data = self._readBlock(f, SONGNAME_OFFSET, SONGNAME_LEN)
+        return self._toString(data)
 
     # information about the samples
     # DON'T CALL DIRECTLY
-    def __loadSampleInfo(self, f: BufferedReader) -> list[Sample]:
+    def _loadSampleInfo(self, f: BufferedReader) -> list[Sample]:
         sample_array = []
         for i in range(self.max_sample_count):
             f.seek(SAMPLEARR_OFFSET + SAMPLEBLOCK_SIZE*i)
-            name = self.__toString(f.read(SAMPLENAME_LEN))
-            length = self.__toInt_BE(f.read(2)) * 2
-            finetune = self.__extractBits(f.read(1), 0, 3) 
-            volume = self.__toInt_LE(f.read(1)) 
-            repeatpoint = self.__toInt_LE(f.read(2)) * 2
-            looplen = self.__toInt_LE(f.read(2)) * 2
+            name = self._toString(f.read(SAMPLENAME_LEN))
+            length = self._toInt_BE(f.read(2)) * 2
+            finetune = self._extractBits(f.read(1), 0, 3) 
+            volume = self._toInt_LE(f.read(1)) 
+            repeatpoint = self._toInt_LE(f.read(2)) * 2
+            looplen = self._toInt_LE(f.read(2)) * 2
 
             sample_array.append(Sample(name, length, finetune, volume, repeatpoint, looplen))
         return sample_array
     
     # the actual sample recordings
-    def __loadSampleData(self, f: BufferedReader) -> list[Sample]:
-        sample_array = self.__loadSampleInfo(f)
+    def _loadSampleData(self, f: BufferedReader) -> list[Sample]:
+        sample_array = self._loadSampleInfo(f)
         
         offset = 0
         sample_count = len(sample_array)
@@ -186,52 +188,52 @@ class Loader:
             sample = sample_array[i]
             base_addr = PATTERNS_OFFSET + self._pattern_count*PATTERN_SIZE
             address = base_addr + offset
-            sample.data = list(self.__readBlock(f, address, sample.length))
+            sample.data = list(self._readBlock(f, address, sample.length))
             offset += sample.length
         return sample_array
 
     # length of the song
-    def __readSongLength(self, f: BufferedReader) -> int:
-        data = self.__readBlock(f, SONGLENGTH_OFFSET, 1)
-        self._song_length = self.__toInt_LE(data)
+    def _readSongLength(self, f: BufferedReader) -> int:
+        data = self._readBlock(f, SONGLENGTH_OFFSET, 1)
+        self._song_length = self._toInt_LE(data)
         return self._song_length
 
     # noisetracker uses this byte for restart before the end of file
-    def __readSearchUntil(self, f: BufferedReader) -> int:
-        data = self.__readBlock(f, SEARCHUNTIL_OFFSET, 1)
-        return self.__toInt_LE(data)
+    def _readSearchUntil(self, f: BufferedReader) -> int:
+        data = self._readBlock(f, SEARCHUNTIL_OFFSET, 1)
+        return self._toInt_LE(data)
 
     # 128 positions that tell the tracker what pattern (0-63) to play at that position (0-127)
-    def __loadPatternPositions(self, f: BufferedReader) -> list[int]:
-        data = list(self.__readBlock(f, PATTERNPOS_OFFSET, PATTERNPOS_LEN))[0:self._song_length]
+    def _loadPatternPositions(self, f: BufferedReader) -> list[int]:
+        data = list(self._readBlock(f, PATTERNPOS_OFFSET, PATTERNPOS_LEN))[0:self._song_length]
         self.pattern_count = max(data) + 1    # save for later
         return data
     
     # the actual note layout and rythm information
     # CALL loadPatternPositions() FIRST, because of pattern_count
-    def __loadPatternData(self, f: BufferedReader) -> list[Pattern]:
+    def _loadPatternData(self, f: BufferedReader) -> list[Pattern]:
         pattern_array = []
         for pattern_idx in range(self.pattern_count):
             pattern = Pattern()
             for channel_idx in range(CHANNEL_COUNT):
-                pattern[channel_idx] = self.__readChannel(f, pattern_idx, channel_idx)
+                pattern[channel_idx] = self._readChannel(f, pattern_idx, channel_idx)
             pattern_array.append(pattern)
         return pattern_array
 
     # reads the magic 4 bytes which indicate the module version
-    def __readMagic(self, f: BufferedReader) -> str:
-        data = self.__readBlock(f, MAGIC_OFFSET, 4)
-        return self.__toString(data)
+    def _readMagic(self, f: BufferedReader) -> str:
+        data = self._readBlock(f, MAGIC_OFFSET, 4)
+        return self._toString(data)
 
     # reads a given channel (0-3) of a given pattern
-    def __readChannel(self, f: BufferedReader, pattern_no:int, channel_no: int) -> list[Note]:
+    def _readChannel(self, f: BufferedReader, pattern_no:int, channel_no: int) -> list[Note]:
         notelist = []
         for note_idx in range(MAX_ROW_COUNT):
             base_addr = PATTERNS_OFFSET + pattern_no*PATTERN_SIZE + channel_no*NOTE_SIZE
             note_addr = base_addr + note_idx*(NOTE_SIZE*4)
-            note_data = self.__readBlock(f, note_addr , 4)
+            note_data = self._readBlock(f, note_addr , 4)
             
-            sample, period, effect = self.__extractNoteInfo(note_data)
+            sample, period, effect = self._extractNoteInfo(note_data)
             notelist.append(Note(sample, period, effect))
             
         return notelist
