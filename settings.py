c46110a9be5ee372492abba88b41e05fbf4c8156
adbik4 <abik@student.agh.edu.pl>
2026-02-15 15:30:27 +0100

    Changed the default settings

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
-FILEPATH = "examples/_yes.mod"           # good example: brainblast - pattern 18, BPM 120 TPB 4
-CHANNELS = [2]                      # choose from [0, 1, 2, 3]
-START_PATTERN, START_NOTE = (0, 0)
+FILEPATH = "examples/brainbla.mod"           # good example: brainblast - pattern 18, BPM 120 TPB 4
+CHANNELS = [0, 1, 2, 3]                      # choose from [0, 1, 2, 3]
+START_PATTERN, START_NOTE = (18, 0)
 BPM = 120
 TPB = 4
 
 PLAYBACK_RATE = 48000
-INTERPOLATION = 'zero_order_hold'   # choose from [zero_order_hold (none), linear, sinc_fastest, sinc_medium, sinc_best]
+INTERPOLATION = 'linear'   # choose from [zero_order_hold (none), linear, sinc_fastest, sinc_medium, sinc_best]
 
-SHOW_VISUALIZER = False
+SHOW_VISUALIZER = True
 USE_PROFILER = False
